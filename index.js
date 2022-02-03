eb0085de561240135a96af1517638a968ca79608
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2022-02-04 01:07:14 +0600

    added node-fetch

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 import fs from 'fs';
 import { Command } from 'commander/esm.mjs';
+import fetch from 'node-fetch';
+const STORAGE_PATH = './storage';
+
+
+// create database.json if not exists
+if (!fs.existsSync(STORAGE_PATH)) {
+    fs.mkdirSync(STORAGE_PATH);
+    fs.writeFileSync(`${STORAGE_PATH}/database.json`, '[]');
+}else{
+    fs.writeFileSync(`${STORAGE_PATH}/database.json`, '[]');
+}
 
 const program = new Command();
 
@@ -13,6 +24,7 @@ program
   .description('add a movie to the watchlist')
   .action((name) => {
     console.log(`Adding ${name} to the watchlist`);
+    //https://omdbapi.com/?t=open%20season%202&apikey=key
   });
 
   
